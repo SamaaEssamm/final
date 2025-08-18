a860a22b7751689f844489cc890e65c05797a9f6
SamaaEssamm <Sama.Essan7421@compit.aun.edu.eg>
2025-08-18 01:30:11 +0000

    edit for deploy

--- chatbot_Gem.py
+++ chatbot_Gem.py
@@ -6,7 +6,10 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Load the full rules text
-rules_text = open("Internal_Regulation_EN.txt", encoding="utf-8").read()
+
+rules_path = os.path.join(os.path.dirname(__file__), "Internal_Regulation_EN.txt")
+rules_text = open(rules_path, encoding="utf-8").read()
+
 
 def ask_rule_question(question):
     prompt = f"""
